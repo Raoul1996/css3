68ccbf7f8d393073573d11085ff9680b67f399f6
Raoul1996 <baobaozhou199606@gmail.com>
2017-07-26 14:59:25 +0000

    What: 修复js不能生效的bug
    Why:
    
    *
    
    How:
    
    * 因为在addEventListener中，touchstart是正确的名称而我使用了小驼峰写法

--- zoomImages/image-zoomer.js
+++ zoomImages/image-zoomer.js
@@ -2,7 +2,7 @@
 
 class ImageZoomer {
   constructor () {
-    this.element = document.querySelector('.zommer')
+    this.element = document.querySelector('.zoomer')
     this.target = document.querySelector('.target')
     this.canvas = document.querySelector('.zoomer__canvas')
     this.ctx = this.canvas.getContext('2d')
@@ -42,8 +42,10 @@ class ImageZoomer {
 
   onStart (evt) {
     if (evt.target !== this.target) return
+    this.x = evt.pageX || evt.touches[0].pageX
+    this.y = evt.pageY || evt.touches[0].pageY
 
-    evt.preventDefault()
+    // evt.preventDefault()
     this.trackingTouch = true
 
     this.targetZoomed = 1
@@ -63,36 +65,39 @@ class ImageZoomer {
     this.trackingTouch = false
 
     this.targetZoomed = 0
+    console.log('haha')
   }
 
   update () {
     const TAU = Math.PI * 2
-    this.trackingTouch = false
+    const MAX_RADIUS = 46
+    const radius = this.zoomed * MAX_RADIUS
 
-    this.zoomed += (this.targetZoomed - this.zoomed) / 10
-
+    this.ctx.clearRect(0,0,128,128)
     this.ctx.fillStyle = '#ffffff'
     this.ctx.beginPath()
-    this.ctx.arc(64,64,64,0, TAU)
+    this.ctx.arc(64, 128 - radius, radius, 0, TAU)
     this.ctx.closePath()
     this.ctx.fill()
 
     this.element.style.transform = `translate(${this.x}px,${this.y}px)`
 
+    this.zoomed += (this.targetZoomed - this.zoomed) / 5
+    this.trackingTouch = false
     requestAnimationFrame(this.update)
 
   }
 
   addEventListeners () {
-    document.addEventListener('touchStart', this.onStart)
-    document.addEventListener('touchMove', this.onMove)
-    document.addEventListener('touchEnd', this.onEnd)
+    document.addEventListener('touchstart', this.onStart)
+    document.addEventListener('touchmove', this.onMove)
+    document.addEventListener('touchend', this.onEnd)
   }
 
   removeEventListeners () {
-    document.removeEventListener('touchStart', this.onStart)
-    document.removeEventListener('touchMove', this.onMove)
-    document.removeEventListener('touchEnd', this.onEnd)
+    document.removeEventListener('touchstart', this.onStart)
+    document.removeEventListener('touchmove', this.onMove)
+    document.removeEventListener('touchend', this.onEnd)
   }
 }
 
