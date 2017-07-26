e5dfeeb877f77e5828bd246d1ba70486d5785217
Raoul1996 <baobaozhou199606@gmail.com>
2017-07-26 14:31:02 +0000

    cant work

--- zoomImages/image-zoomer.js
+++ zoomImages/image-zoomer.js
@@ -4,7 +4,7 @@ class ImageZoomer {
   constructor () {
     this.element = document.querySelector('.zommer')
     this.target = document.querySelector('.target')
-    this.canvas = document.querySelector('.zoomer-canvas')
+    this.canvas = document.querySelector('.zoomer__canvas')
     this.ctx = this.canvas.getContext('2d')
 
     this.onStart = this.onStart.bind(this)
@@ -12,12 +12,32 @@ class ImageZoomer {
     this.onEnd = this.onEnd.bind(this)
     this.update = this.update.bind(this)
 
+    this.zoomed = 0
+    this.targetZoomed = 0
+
     this.x = 0
     this.y = 0
     this.trackingTouch = false
     this.scheduledUpdate = false
 
+    this.initCanvas()
     this.addEventListeners()
+    requestAnimationFrame(this.update)
+
+  }
+
+  initCanvas () {
+    const width = 128
+    const height = 128
+    const dPR = window.devicePixelRatio || 1
+
+    this.canvas.width = width * dPR
+    this.canvas.height = height * dPR
+
+    this.canvas.style.width = `${width}px`
+    this.canvas.style.height = `${height}px`
+
+    this.ctx.scale(dPR, dPR)
   }
 
   onStart (evt) {
@@ -25,12 +45,14 @@ class ImageZoomer {
 
     evt.preventDefault()
     this.trackingTouch = true
+
+    this.targetZoomed = 1
   }
 
   onMove (evt) {
     if (!this.trackingTouch) return
-    this.x = evt.touches[0].pageX
-    this.y = evt.touches[0].pageY
+    this.x = evt.pageX || evt.touches[0].pageX
+    this.y = evt.pageY || evt.touches[0].pageY
 
     if (this.scheduledUpdate) return
     this.scheduledUpdate = true
@@ -39,11 +61,26 @@ class ImageZoomer {
 
   onEnd () {
     this.trackingTouch = false
+
+    this.targetZoomed = 0
   }
 
   update () {
-  this.trackingTouch = false
+    const TAU = Math.PI * 2
+    this.trackingTouch = false
+
+    this.zoomed += (this.targetZoomed - this.zoomed) / 10
+
+    this.ctx.fillStyle = '#ffffff'
+    this.ctx.beginPath()
+    this.ctx.arc(64,64,64,0, TAU)
+    this.ctx.closePath()
+    this.ctx.fill()
+
     this.element.style.transform = `translate(${this.x}px,${this.y}px)`
+
+    requestAnimationFrame(this.update)
+
   }
 
   addEventListeners () {
@@ -59,4 +96,6 @@ class ImageZoomer {
   }
 }
 
-new ImageZoomer()
+window.addEventListener('load', () => {
+  new ImageZoomer()
+})
